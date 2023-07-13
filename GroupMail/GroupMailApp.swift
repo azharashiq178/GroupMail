495d40a6f566c2a8679239c41104594beda6a589
m.azher.ext@tahaluf.ae <Muhammad Azher>
2023-07-13 10:29:24 +0400

    Initial Commit

---
+++ GroupMail/GroupMailApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupMailApp.swift
+//  GroupMail
+//
+//  Created by muhammad azher on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GroupMailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
